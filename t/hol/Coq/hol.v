48f237d7b45ebf030494c402e4857252f044b663
Chantal Keller <artemis@delos.(none)>
2010-06-29 12:03:25 +0200

    Règle eq_mp

--- t/hol/Coq/hol.v
+++ t/hol/Coq/hol.v
@@ -58,7 +58,7 @@ Hint Rewrite ax5 : re.
 Parameter eps: hterm o -> Type.
 
 Axiom ax6 : forall a: hterm o,
-forall b: hterm o, eps (himp a b) = eps a -> eps b.
+forall b: hterm o, eps (himp a b) = (eps a -> eps b).
 
 Hint Rewrite ax6 : re.
 
@@ -82,56 +82,37 @@ Parameter fun_ext: forall a: htype, forall b: htype, forall f: hterm (arrow a b)
 Parameter prop_ext: forall p: hterm o, forall q: hterm o, eps (himp p q) -> eps (himp q p) -> eps (hequiv p q).
 
 
-(* Beta rule *)
+(* Rules *)
+
+Definition refl: forall a: htype, forall x: hterm a, eps (heq a x x).
+intros a x. autorewrite with re. intros P h. exact h.
+Defined.
+
+
+Definition sym: forall a: htype, forall x: hterm a, forall y: hterm a, eps (heq a x y) -> eps (heq a y x).
+intros a x y Hxy. autorewrite with re in *. intros P Hy.
+pose (H1 := (Hxy (Lam a o (fun z: hterm a => himp (App a o P z) (App a o P x))))). autorewrite with re in H1. exact (H1 (fun H: eps (App a o P x) => H) Hy).
+Defined.
+
+
+Definition trans: forall a: htype, forall s: hterm a, forall t: hterm a, forall u: hterm a, eps (heq a s t) -> eps (heq a t u) -> eps (heq a s u).
+intros a s t u Hst Htu. autorewrite with re in *. intros P Hs. exact (Htu P (Hst P Hs)).
+Defined.
+
 
 Definition beta: forall a: htype, forall b: htype, forall f: (hterm a -> hterm b), forall u: hterm a, eps (heq b (App a b (Lam a b f) u) (f u)).
 intros a b f u. autorewrite with re. intros P H. exact H.
 Defined.
 
 
-(* ;; Rules *)
-
-(* refl: a: htype -> x: hterm a -> eps (heq a x x). *)
-(* [a: htype, *)
-(*  x: hterm a] refl a x --> P: hterm (arrow a o) => h: eps (App a o P x) => h. *)
-
-
-(* sym: a: htype -> x: hterm a -> y: hterm a -> eps (heq a x y) -> eps (heq a y x). *)
-(* [a: htype, *)
-(*  x: hterm a, *)
-(*  y: hterm a, *)
-(*  Hxy: eps (heq a x y)] *)
-(* sym a x y Hxy --> P: hterm (arrow a o) => Hy: eps (App a o P y) => Hxy (Lam a o (z: hterm a => himp (App a o P z) (App a o P x))) (H: eps (App a o P x) => H) Hy. *)
+Definition mk_comb: forall a: htype, forall b: htype, forall s: hterm (arrow a b), forall t: hterm (arrow a b), forall u: hterm a, forall v: hterm a,
+  eps (heq (arrow a b) s t) -> eps (heq a u v) -> eps (heq b (App a b s u) (App a b t v)).
+intros a b s t u v Hst Huv. autorewrite with re in *. intros P Hsu.
+pose (H1 := Hst (Lam (arrow a b) o (fun s: hterm (arrow a b) => App b o P (App a b s v)))). autorewrite with re in H1. apply H1. pose (H2 := (Huv (Lam a o (fun u: hterm a => App b o P (App a b s u))))). autorewrite with re in H2. apply H2. exact Hsu.
+Defined.
 
 
-(* trans: a: htype -> s: hterm a -> t: hterm a -> u: hterm a -> eps (heq a s t) -> eps (heq a t u) -> eps (heq a s u). *)
-(* [a: htype, *)
-(*  s: hterm a, *)
-(*  t: hterm a, *)
-(*  u: hterm a, *)
-(*  Hst: P: hterm (arrow a o) -> eps (App a o P s) -> eps (App a o P t), *)
-(*  Htu: P: hterm (arrow a o) -> eps (App a o P t) -> eps (App a o P u)] *)
-(* trans a s t u Hst Htu --> P: hterm (arrow a o) => Hs: eps (App a o P s) => Htu P (Hst P Hs). *)
-
-
-(* mk_comb: a: htype -> b: htype -> s: hterm (arrow a b) -> t: hterm (arrow a b) -> u: hterm a -> v: hterm a -> *)
-(*   eps (heq (arrow a b) s t) -> eps (heq a u v) -> eps (heq b (App a b s u) (App a b t v)). *)
-(* ;; [a: htype, *)
-(* ;;  b: htype, *)
-(* ;;  s: hterm (arrow a b), *)
-(* ;;  t: hterm (arrow a b), *)
-(* ;;  u: hterm a, *)
-(* ;;  v: hterm a, *)
-(* ;;  Hst: eps (heq (arrow a b) s t), ; = P: hterm (arrow (arrow a b) o) -> Hs: eps (App (arrow a b) o P s) -> eps (App (arrow a b) o P y) *)
-(* ;;  Huv: eps (heq a u v)] ; = P: hterm (arrow a o) -> Hu: eps (App a o P u) -> eps (App a o P v) *)
-(* ;; mk_comb a b s t u v Hst Huv --> P: hterm (arrow b o) => Hsu: eps (App b o P (App a b s u)) => *)
-(* ;;   ; eps (App b o P (App a b t v)) *)
-
-
-(* ;; Abs cannot be written (meta-rule), we can only write instances of it *)
-
-
-(* ;; Liste des règles de HOL-Light qui fonctionnent : *)
-(* ;; - refl *)
-(* ;; - trans *)
-(* ;; - sym *)
+Definition eq_mp : forall p: hterm o, forall q: hterm o,
+  eps (hequiv p q) -> eps p -> eps q.
+intros p q Hpq Hp. rewrite <- ax5 in Hpq. rewrite <- ax4 in Hpq. autorewrite with re in *. pose (H := Hpq (Lam o o (fun q' => q'))). autorewrite with re in H. exact (H Hp).
+Defined.
